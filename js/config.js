16bca141fb8a5a219a3cc1aee21bed7309dccf82
xiongjian <jianxiong_@outlook.com>
2025-05-09 00:40:57 +0800

    update

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const siteConfig = {
     // Personal information sections
     about: true,           // About Me section
-    research: false,       // Research Interests section
+    research: true,       // Research Interests section
     publications: false,    // Publications section
     projects: false,       // Research Projects section
     education: true,       // Education section
